742582a1455492455d6c85c5b8c1aac696e1df5f
Mikhail Oliveira <mikhail.oliveira@funceme.br>
2015-05-07 15:20:38 -0300

    Ajuste na Funcao VAP

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ __date__ = '13/04/2015'
 import os
 import pyart
 import math
+import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 
 __file__ = "/usr/local/lib/python2.7/dist-packages/PyFuncemeClimateTools/"
 
+
 def plotImage(radar, localdir):
     """
     Funcao para Plotar Imagem do Radar
-    :param radar: Objeto Radar
+    Plot function for Radar Image
+    :param localdir: Directory of PyFuncemeClimateTools - Diretorio do PyFuncemeClimateTools
+    :param radar: Radar Object - Objeto Radar
     :return:
     """
-
     # map = Basemap(projection='cyl',
     #               llcrnrlat=-9.6,
     #               urcrnrlat=-0.9,
@@ -30,7 +33,7 @@ def plotImage(radar, localdir):
     #
     # map.readshapefile(localdir + '/shp/ceara', 'world', drawbounds=True, linewidth=.5, color='k')
 
-    #create the plot using RadarMapDisplay (recommended method)
+    #create the plot using RadarMapDisplay
     display = pyart.graph.RadarMapDisplay(radar)
     display.plot_ppi_map('velocity', 0, vmin=-10., vmax=10.,
                          min_lat=-9.6, max_lat=-0.9,
@@ -42,10 +45,12 @@ def plotImage(radar, localdir):
     plt.show()
     #plt.savefig('Radar_Quixeramobim_Band_S - Product VAP.png', format='png')
 
+
 def read_radar():
     """
     Leitura do arquivo bruto (Sigmet) do Radar
-    :return: Objeto Radar com todas as suas Caracteristicas
+    Reading the raw file (Sigmet) Radar
+    :return: Radar object with all its features - Objeto Radar com todas as suas Caracteristicas
     """
     filename = 'XXX140331061059.RAW1JMJ'
     local_dir = os.path.dirname(__file__)
@@ -57,28 +62,28 @@ def read_radar():
 def vap(radar):
     """
     Implementacao da tecnica de Processamento VAP (Velocity-Azimuth Processing)
-    :param radar:
+    Implementation of the technical processing of VAP (Velocity-Azimuth Processing)
+    :param radar: Radar object - Objeto Radar
     :return:
     """
-
     #The horizontal wind components u and v
-    u, v = []
-    for teta in range(len(radar.azimuth['data'])):
-        u_1 = 1 * math.cos(radar.azimuth['data'][teta] - (radar.azimuth['data'][teta] - radar.azimuth['data'][teta+1]))
-        u_2 = 1 * math.cos(radar.azimuth['data'][teta] + (radar.azimuth['data'][teta] - radar.azimuth['data'][teta+1]))
-        u_3 = math.sin((2 * (radar.azimuth['data'][teta] - radar.azimuth['data'][teta+1])))
-        u[teta] = (u_1 - u_2) / u_3
+    u = np.zeros(3600)
+    v = np.zeros(3600)
+    for i in range(len(radar.azimuth['data'])):
 
-        v_1 = 1 * math.sin(radar.azimuth['data'][teta] + (radar.azimuth['data'][teta] - radar.azimuth['data'][teta+1]))
-        v_2 = 1 * math.sin(radar.azimuth['data'][teta] - (radar.azimuth['data'][teta] - radar.azimuth['data'][teta+1]))
-        v_3 = math.sin((2 * (radar.azimuth['data'][teta] - radar.azimuth['data'][teta+1])))
-        v[teta] = (v_1 - v_2) / v_3
+        #radar.azimuth['data'][i+1] - radar.azimuth['data'][i] = 1
+        u_1 = 1 * math.cos(radar.azimuth['data'][i] - 1)
+        u_2 = 1 * math.cos(radar.azimuth['data'][i] + 1)
+        u[i] = (u_1 - u_2) / (math.sin(2))
 
+        v_1 = 1 * math.sin(radar.azimuth['data'][i] + 1)
+        v_2 = 1 * math.sin(radar.azimuth['data'][i] - 1)
+        v[i] = (v_1 - v_2) / (math.sin(2))
 
     return
 
 
 if __name__ == '__main__':
     radar = read_radar()
-    #vap(radar)
+    vap(radar)
     plotImage(radar, __file__)
